cbf84dc7b4e6938ffe7181310aa747a40904ab09
laurent buhler <lbuhler@gmail.com>
2020-06-07 17:11:39 -0700

    serialize and deserialize

--- src/main/java/org/example/ressources/TopLevel.java
+++ src/main/java/org/example/ressources/TopLevel.java
@@ -1,14 +1,22 @@
 package org.example.ressources;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
 import java.util.List;
 import java.util.Optional;
 
 @Value.Immutable
+@JsonSerialize
+//@JsonDeserialize(as = AttemptInternalBuilder.ImmutableAttempt.class)
 public abstract class TopLevel {
+    @JsonProperty("name")
     public abstract String name();
+    @JsonProperty("sub_name")
     public abstract Optional<String> subName();
+    @JsonProperty("code")
     public abstract Integer code();
+    @JsonProperty("urls")
     public abstract List<SecondLevel> urls();
 }
--- src/test/java/org/example/ressources/TopLevelTest.java
+++ src/test/java/org/example/ressources/TopLevelTest.java
@@ -1,17 +1,43 @@
 package org.example.ressources;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TopLevelTest {
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setup() {
+        objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new Jdk8Module());
+
+    }
 
     @Test
-    public void testBuilder() {
+    public void testBuilder() throws JsonProcessingException {
         ImmutableTopLevel obj = ImmutableTopLevel.builder()
                 .code(1)
                 .name("lol")
                 .build();
+
+        String json = objectMapper.writeValueAsString(obj);
+        Assertions.assertThat(obj.name()).isEqualTo("lol");
+        Assertions.assertThat(obj.subName()).isEmpty();
+
+    }
+
+    @Test
+    public void testBuilderFromString() throws JsonProcessingException {
+        String json = "{\"name\":\"lol\",\"sub_name\":null,\"code\":1,\"urls\":[]}";
+
+        ImmutableTopLevel obj = objectMapper.readValue(json, ImmutableTopLevel.class);
+
         Assertions.assertThat(obj.name()).isEqualTo("lol");
         Assertions.assertThat(obj.subName()).isEmpty();
 
